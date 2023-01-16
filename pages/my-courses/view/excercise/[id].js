5beeb492c812bd471652fb3e97ee52e055b11d43
Ivan Golemdzhiyski <jackdye500@gmail.com>
2023-01-16 19:39:02 +0000

    Created Iframe that displays the code

--- pages/my-courses/view/excercise/[id].js
+++ pages/my-courses/view/excercise/[id].js
@@ -1,16 +1,9 @@
 import React from "react";
 import PageBanner from "@/components/Common/PageBanner";
-import Link from "next/link";
+
 import { parseCookies } from "nookies";
 import axios from "axios";
 import baseUrl from "@/utils/baseUrl";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionItemHeading,
-  AccordionItemButton,
-  AccordionItemPanel,
-} from "react-accessible-accordion";
 
 const SingleCourses = ({ sections }) => {
   const [videoId, setVideoId] = React.useState(
@@ -27,86 +20,7 @@ const SingleCourses = ({ sections }) => {
           sections.length ? sections[0].course.title : "No Videos"
         }
       />
-
-      <div className="ptb-100">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-3">
-              <div className="course-video-list">
-                {sections.length ? (
-                  sections.map((section) => (
-                    <Accordion key={section.id}>
-                      <AccordionItem>
-                        <AccordionItemHeading>
-                          <AccordionItemButton>
-                            {section.name}
-                          </AccordionItemButton>
-                        </AccordionItemHeading>
-                        <AccordionItemPanel>
-                          {section.videos.length ? (
-                            section.videos.map((video) => (
-                              <div key={video.id}>
-                                <Link
-                                  legacyBehavior
-                                  key={video.id}
-                                  href="/my-courses/[videos]/[id]"
-                                  as={`/my-courses/${section.course.id}/${video.id}`}
-                                >
-                                  <a
-                                    onClick={(e) => {
-                                      e.preventDefault();
-                                      setVideoId(video.video_url);
-                                    }}
-                                  >
-                                    <img
-                                      src={section.course.profilePhoto}
-                                      alt={section.course.title}
-                                    />
-                                    <h4>{video.name}</h4>
-                                  </a>
-                                </Link>
-                              </div>
-                            ))
-                          ) : (
-                            <h3>No Videos</h3>
-                          )}
-                          <div style={{ marginTop: "100px" }}></div>
-                          {section.quizzes.length &&
-                            section.quizzes.map((quiz) => (
-                              <div key={quiz.id}>
-                                <Link
-                                  legacyBehavior
-                                  key={quiz.id}
-                                  href="/my-courses/[quizzes]/[id]"
-                                  as={`/my-courses/quiz/${quiz.id}`}
-                                >
-                                  <a href={`/my-courses/quiz/${quiz.id}`}>
-                                    <h4>{quiz.name}</h4>
-                                  </a>
-                                </Link>
-                              </div>
-                            ))}
-                        </AccordionItemPanel>
-                      </AccordionItem>
-                    </Accordion>
-                  ))
-                ) : (
-                  <h3>No Sections</h3>
-                )}
-              </div>
-            </div>
-
-            <div className="col-lg-9">
-              <div className="course-video-iframe">
-                <video key={videoId} controls>
-                  <source src={videoId} type="video/mp4" />
-                  <source src="/images/courses/courses5.jpg" type="video/ogg" />
-                </video>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <iframe width="100%" height="700" src={"http://localhost:3001/"} />
     </React.Fragment>
   );
 };
