842d4235b037363e140969ca73aa75be99f50400
Ivan Golemdzhiyski <jackdye500@gmail.com>
2023-04-11 15:31:20 +0100

    removed last / in url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://frontend-nine-iota-87.vercel.app/"
+    ? "https://frontend-nine-iota-87.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
