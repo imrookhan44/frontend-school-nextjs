c8dfb9bf58f255113247f1805a33c2e0f7c6f052
Ivan Golemdzhiyski <jackdye500@gmail.com>
2023-04-11 09:00:36 +0100

    Removed pwa setting

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const path = require("path");
-const withPWA = require("next-pwa");
-const nextTranslate = require("next-translate");
 
 module.exports = {
-  // pwa: {
-  //   // disable: process.env.NODE_ENV === "development",
-  //   // dest: 'public',
-  //   register: true,
-  //   sw: "/sw.js",
-  // },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
