88b2113db7a62b4760d7a070c4ba240fb4af2786
Ivan Golemdzhiyski <jackdye500@gmail.com>
2023-04-11 15:23:14 +0100

    fixed based url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,6 @@
-const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://edemy-react.envytheme.com' 
-: 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://frontend-nine-iota-87.vercel.app/"
+    : "http://localhost:3000";
 
 export default baseUrl;
