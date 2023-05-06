26cf92e79f02475c8cb679ff7b77c54f2ff42ed8
Ivan Golemdzhiyski <jackdye500@gmail.com>
2023-05-06 16:49:08 +0100

    cleaned out some unused pages

--- pages/index.js
+++ pages/index.js
@@ -11,35 +11,39 @@ import Ad from "@/components/Index/Ad";
 import Funfacts from "@/components/Index/Funfacts";
 import Blog from "@/components/Index/Blog";
 import EdemyPremium from "@/components/Index/EdemyPremium";
+import HowToApply from "@/components/Index/HowToApply";
+import SignUpForm from "@/components/Index/SignUpForm";
 import axios from "axios";
 import baseUrl from "@/utils/baseUrl";
 import SEO from "@/components/SEO";
 
 const Index = ({ courses }) => {
-	const { t } = useTranslation("distance-learning");
-	return (
-		<>
-			<SEO title={t("pagetitle")} description={t("bannersubtitle")} />
-			<MainBanner />
-			<Features />
-			<TopCourses courses={courses} />
-			<About />
-			<Testimonials />
-			<Instance />
-			<Partner />
-			<Ad />
-			<Funfacts />
-			<Blog />
-			<EdemyPremium />
-		</>
-	);
+  const { t } = useTranslation("distance-learning");
+  return (
+    <>
+      <SEO title={t("pagetitle")} description={t("bannersubtitle")} />
+      <MainBanner />
+      <Features />
+      <TopCourses courses={courses} />
+      <About />
+      <Testimonials />
+      <Instance />
+      <Partner />
+      <HowToApply />
+      <SignUpForm />
+      <Ad />
+      <Funfacts />
+      <Blog />
+      <EdemyPremium />
+    </>
+  );
 };
 
 Index.getInitialProps = async () => {
-	const url = `${baseUrl}/api/v1/courses/homepage-courses`;
-	const response = await axios.get(url);
-	// console.log(response)
-	return response.data;
+  const url = `${baseUrl}/api/v1/courses/homepage-courses`;
+  const response = await axios.get(url);
+  // console.log(response)
+  return response.data;
 };
 
 export default Index;
